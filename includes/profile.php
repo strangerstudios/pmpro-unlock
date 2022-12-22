8f47a91cdd33f547ca935d614aaf802b0f5f5e35
Andrew Lima <andrew@arctek.co.za>
2022-12-22 15:51:18 +0200

    Ability to remove wallet address for a user.
    * Enhancement: Ability to remove wallet from user.

--- includes/profile.php
+++ includes/profile.php
@@ -3,6 +3,12 @@
  * Profile page.
  */
 
+/**
+ * Functionality for user profile page and connected/removing wallet address.
+ * 
+ * @param object $user User object.
+ *  @since 1.0
+ */
 function pmproup_profile_connect_wallet( $user ) {
 
     // Let's try to check and save it on page load.
@@ -13,8 +19,42 @@ function pmproup_profile_connect_wallet( $user ) {
     }
 
     if ( ! is_wp_error( $wallet_address ) && ! empty( $wallet_address ) ) {
-        echo "<p>" . sprintf( esc_html__( "Your Wallet Address is: %s", 'pmpro-unlock' ), esc_html( $wallet_address ) ) . "</p>";
+        echo "<p>" . sprintf( esc_html__( "Your Wallet Address is: %s", 'pmpro-unlock' ), '<span id="pmpro_unlock_wallet_address">' . esc_html( $wallet_address ) . '</span>' ) . ' ';
+        echo '<a style="color: red;" id="pmpro_unlock_remove_wallet" class="pmpro_unlock_remove" href="javascript:void(0);">' . esc_html__( 'remove', 'pmpro-addon-packages' ) . '</a>';
+        echo '<input type="hidden" id="pmpro_unlock_delete_wallet" name="pmpro_unlock_delete_wallet" value="" />';
+        echo "</p>";
+
+        ?>
+        <script>
+            jQuery(document).ready(function(){
+                jQuery('#pmpro_unlock_remove_wallet').on('click', function(){
+                    jQuery('#pmpro_unlock_remove_wallet').css('text-decoration', 'line-through');
+                    jQuery('#pmpro_unlock_wallet_address').css('text-decoration', 'line-through');
+
+                    jQuery('#pmpro_unlock_delete_wallet').val('1');
+                });
+            });
+        </script>
+        <?php
     }
 }
 add_action( 'pmpro_show_user_profile', 'pmproup_profile_connect_wallet', 10, 1 );
 add_action( 'pmpro_after_membership_level_profile_fields', 'pmproup_profile_connect_wallet', 10, 1 );
+
+/**
+ * Delete wallet address if remove option is selected.
+ * 
+ * @since TBD
+ */
+function pmproup_profile_remove_wallet() {
+    if ( isset( $_REQUEST['pmpro_unlock_delete_wallet'] ) && ! empty( $_REQUEST['pmpro_unlock_delete_wallet'] ) ) {
+
+        if ( ! empty( $_REQUEST['user_id'] ) ) {
+			$user_id = intval( $_REQUEST['user_id'] );
+		}
+
+        delete_user_meta( $user_id, 'pmproup_wallet' );
+    }
+
+}
+add_action( 'profile_update', 'pmproup_profile_remove_wallet' );
